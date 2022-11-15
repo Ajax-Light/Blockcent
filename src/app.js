5fc774665f302a4cde5be18699f7e3f5161b40b9
Ujwal Kundur <ujwal.kundur@gmail.com>
2022-11-15 19:58:05 +0530

    Feat: Add gateway imports to app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-const server = require('./api/server')
+// Fabric Gateway Client
+const gateway = require('./gateway');
+
+// Express
+const server = require('./api/server');
 
 // Init Blockchain connection
-// TODO
-console.log("Connection to Fabric Gateway established")
+gateway.start();
+console.log("Connection to Fabric Gateway established");
 
 // Start Express Server
-console.log("Start express server")
-server.start()
+console.log("Start express server");
+server.start();
 
 // Graceful shutdown
-console.log("Express server shutdown, Gateway connection closed")
+server.stop();
+gateway.stop();
+console.log("Express server shutdown");
