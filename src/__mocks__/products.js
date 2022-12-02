85e92096b4578345c845e54d576884bf1c9c50a2
Ujwal Kundur <ujwal.kundur@gmail.com>
2022-12-02 22:27:24 +0530

    Feat: Customize Product Descriptions

--- src/__mocks__/products.js
+++ src/__mocks__/products.js
@@ -3,50 +3,50 @@ import { v4 as uuid } from 'uuid';
 export const products = [
   {
     id: uuid(),
-    createdAt: '27/03/2019',
-    description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
+    createdAt: '02/12/2022',
+    description: 'Order Tea / Coffee from the Canteen.',
     media: '/static/images/products/product_1.png',
-    title: 'Dropbox',
-    totalDownloads: '594'
+    title: 'Tea/Coffee',
+    worth: '50'
   },
   {
     id: uuid(),
-    createdAt: '31/03/2019',
-    description: 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
+    createdAt: '02/12/2022',
+    description: 'Order a Fresh Sandwich from the Canteen.',
     media: '/static/images/products/product_2.png',
-    title: 'Medium Corporation',
-    totalDownloads: '625'
+    title: 'Sandwich',
+    worth: '100'
   },
   {
     id: uuid(),
-    createdAt: '03/04/2019',
-    description: 'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
+    createdAt: '02/12/2022',
+    description: 'Order a Fresh Burger from the Canteen.',
     media: '/static/images/products/product_3.png',
-    title: 'Slack',
-    totalDownloads: '857'
+    title: 'Burger',
+    worth: '125'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    description: 'Lyft is an on-demand transportation company based in San Francisco, California.',
+    createdAt: '02/12/2022',
+    description: 'Approve a Due extension for returning books to the library.',
     media: '/static/images/products/product_4.png',
-    title: 'Lyft',
-    totalDownloads: '406'
+    title: 'Library Due Extension',
+    worth: '200'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    description: 'GitHub is a web-based hosting service for version control of code using Git.',
+    createdAt: '02/12/2022',
+    description: 'Request a Letter of Recommendation from a professor.',
     media: '/static/images/products/product_5.png',
-    title: 'GitHub',
-    totalDownloads: '835'
+    title: 'Letter of Recommendation',
+    worth: '1000'
   },
   {
     id: uuid(),
-    createdAt: '04/04/2019',
-    description: 'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
+    createdAt: '02/12/2022',
+    description: 'Request Internship recommendations from a professor.',
     media: '/static/images/products/product_6.png',
-    title: 'Squarespace',
-    totalDownloads: '835'
+    title: 'Internship',
+    worth: '5000'
   }
 ];
